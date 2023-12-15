c1272dec8a8393e452e5210ef8db94fb2bcb8d8a
valkyrie_pilot <valk@randomairborne.dev>
2023-12-15 01:03:14 +0000

    Update to support auto-update and pruning

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, sync::Arc};
+use std::{collections::HashMap, sync::Arc, time::Duration};
 
 use axum::{
     extract::{Path, State},
@@ -6,6 +6,7 @@ use axum::{
     http::StatusCode,
     TypedHeader,
 };
+use tokio::{select, task::JoinSet, time::MissedTickBehavior};
 
 #[tokio::main]
 async fn main() {
@@ -15,10 +16,18 @@ async fn main() {
     let config_str =
         std::fs::read_to_string(cfg_path).expect("Expected config to exist and be valid utf-8");
     let config: Config = toml::from_str(&config_str).expect("Invalid config toml");
+    let config = Arc::new(config);
+    let mut workers = JoinSet::new();
+    if let Some(secs) = config.force_update_interval {
+        workers.spawn(restart_all(secs, config.clone()));
+    }
+    if let Some(secs) = config.prune_interval {
+        workers.spawn(prune(secs));
+    }
     let port = config.port;
     let router = axum::Router::new()
-        .route("/:path", axum::routing::any(restart))
-        .with_state(Arc::new(config));
+        .route("/:path", axum::routing::any(restart_web))
+        .with_state(config);
     axum::Server::bind(&([0, 0, 0, 0], port).into())
         .serve(router.into_make_service())
         .with_graceful_shutdown(async {
@@ -26,9 +35,14 @@ async fn main() {
         })
         .await
         .unwrap();
+    while let Some(val) = workers.join_next().await {
+        if let Err(err) = val {
+            eprintln!("Error on shutdown: {err:?}");
+        }
+    }
 }
 
-async fn restart(
+async fn restart_web(
     Path(name): Path<String>,
     State(state): State<Arc<Config>>,
     TypedHeader(Authorization(auth)): TypedHeader<Authorization<Bearer>>,
@@ -36,8 +50,34 @@ async fn restart(
     if state.token != auth.token() {
         return Err(Error::Unauthorized);
     }
-    let Some(composition) = state.extra.get(&name) else {
-        return Err(Error::NoComposition(name));
+    if let Err(source) = restart(&name, state).await {
+        eprintln!("Error: {source:?}");
+        Err(source)
+    } else {
+        Ok((StatusCode::OK, "Success\n"))
+    }
+}
+
+async fn restart_all(secs: u64, config: Arc<Config>) {
+    let period = Duration::from_secs(secs);
+    let mut ticker = tokio::time::interval(period);
+    ticker.set_missed_tick_behavior(MissedTickBehavior::Delay);
+    loop {
+        select! {
+            _ = tokio::signal::ctrl_c() => break,
+            _ = ticker.tick() => {}
+        };
+        for name in config.extra.keys() {
+            if let Err(source) = restart(name, config.clone()).await {
+                eprintln!("error: {source:?}")
+            }
+        }
+    }
+}
+
+async fn restart(name: &str, config: Arc<Config>) -> Result<(StatusCode, &'static str), Error> {
+    let Some(composition) = config.extra.get(name) else {
+        return Err(Error::NoComposition(name.to_owned()));
     };
     let pull_task = tokio::process::Command::new("docker")
         .arg("compose")
@@ -58,24 +98,57 @@ async fn restart(
     }
 }
 
+async fn prune(secs: u64) {
+    let period = Duration::from_secs(secs);
+    let mut ticker = tokio::time::interval(period);
+    ticker.set_missed_tick_behavior(MissedTickBehavior::Delay);
+    loop {
+        select! {
+            _ = tokio::signal::ctrl_c() => break,
+            _ = ticker.tick() => {}
+        };
+        if let Err(source) = do_prune().await {
+            eprintln!("error: {source:?}")
+        }
+    }
+}
+
+async fn do_prune() -> Result<(), Error> {
+    let pull_task = tokio::process::Command::new("docker")
+        .arg("image")
+        .arg("prune")
+        .arg("-a")
+        .arg("-f")
+        .spawn()?;
+    let output = pull_task.wait_with_output().await?;
+    if !output.status.success() {
+        Err(Error::PruneFailed {
+            stdout: String::from_utf8_lossy(&output.stdout).into(),
+            stderr: String::from_utf8_lossy(&output.stderr).into(),
+        })
+    } else {
+        Ok(())
+    }
+}
+
 #[derive(serde::Deserialize)]
 pub struct Config {
     #[serde(default = "default_port")]
     port: u16,
     token: String,
-    force_update_interval: Option<usize>,
-    prune_interval: Option<usize>,
+    force_update_interval: Option<u64>,
+    prune_interval: Option<u64>,
     #[serde(flatten)]
     extra: HashMap<String, ManagedComposition>,
-}
-
-fn default_port() -> u16 {
-    8080
 }
 
 #[derive(serde::Deserialize)]
 pub struct ManagedComposition {
     work: String,
+}
+
+fn default_port() -> u16 {
+    8080
 }
 
 #[derive(Debug, thiserror::Error)]
@@ -84,21 +157,21 @@ pub enum Error {
     Io(#[from] std::io::Error),
     #[error("Docker pull failed\n")]
     PullFailed { stdout: String, stderr: String },
-    #[error("Unauthorized user attempted to access server\n")]
-    Unauthorized,
+    #[error("Docker prune failed\n")]
+    PruneFailed { stdout: String, stderr: String },
     #[error("No composition found for path `{0}`\n")]
     NoComposition(String),
+    #[error("Unauthorized user attempted to access server\n")]
+    Unauthorized,
 }
 
 impl axum::response::IntoResponse for Error {
     fn into_response(self) -> axum::response::Response {
         eprintln!("{self:?}");
         let status = match self {
-            Error::Io(_) => StatusCode::INTERNAL_SERVER_ERROR,
-            Error::PullFailed {
-                stdout: _,
-                stderr: _,
-            } => StatusCode::INTERNAL_SERVER_ERROR,
+            Error::Io(_) | Error::PullFailed { .. } | Error::PruneFailed { .. } => {
+                StatusCode::INTERNAL_SERVER_ERROR
+            }
             Error::Unauthorized => StatusCode::UNAUTHORIZED,
             Error::NoComposition(_) => StatusCode::NOT_FOUND,
         };
